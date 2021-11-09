c82951beacc1a8bcf45385b4d9d61a47b01693f8
kuherrn <72862745+kuherrn@users.noreply.github.com>
2021-11-09 14:07:17 -0400

    Add files via upload

---
+++ TwoToOne.java
@@ -0,0 +1,31 @@
+
+//https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/java
+//Core Code: https://stackoverflow.com/questions/4989091/removing-duplicates-from-a-string-in-java
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+class TwoToOne {
+    public static String longest (String s1, String s2) {
+
+        String myString = s1 + s2;
+        char[] myArray = myString.toCharArray();
+        Arrays.sort(myArray);
+        Set<Character> charSet = new LinkedHashSet<>();
+        for (char myChar : myArray) {
+            charSet.add(myChar);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(Character c : charSet) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longest("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz"));
+    }
+}
