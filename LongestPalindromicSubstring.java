8ff71383c8bb85c1d24828127c78f7136efd0377
Kuherrn Nagayaindran <72862745+kuherrn@users.noreply.github.com>
2021-11-19 00:42:56 -0400

    Leetcode solution with quadratic runtime

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -33,5 +33,41 @@ class LongestPalindromicSubstring {
             return "" + s.charAt(0);
         }
     }
-    
 }
+
+/*
+n^2 time complexity solution from Leetcode
+
+1. Loop through the array of strings.
+2. During each iteration, expand a window from the middle checking for
+    - charAt equals a left and right pointer
+3. Return the length of the longest palindrome for the iteration
+4. If the length is greater than the current end-start index
+    - update the start index and end index of the substring 
+5. Return the substring
+
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+*/
+    
